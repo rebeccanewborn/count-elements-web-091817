04c63b6e9cf73aa94476ce4d3e70ba45cb16deda
rebeccanewborn <rjn58@cornell.edu>
2017-09-20 11:11:52 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
   # code goes here
+  result_hash = {}
+  array.each do |e|
+    if result_hash[e] == nil
+      result_hash[e] = 1
+    else
+      result_hash[e] += 1
+    end
+  end
+  result_hash
 end
- 
